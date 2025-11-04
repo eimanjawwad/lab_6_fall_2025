f79a842d51b9c3f0c41b2f5d6bfb46f9e73c79cc
Gavin Griffin <gavinlg@stanford.edu>
2025-11-03 20:37:52 -0800

    prompt shortening

--- pupper_llm/realtime_voice.py
+++ pupper_llm/realtime_voice.py
@@ -93,12 +93,12 @@ calls. The tool calls you can perform are: [move forward], [move backward], [mov
 Plan these tool calls in the optimal order needed to execute the command - for example, if the command is "move forward, then turn left, then bark", the order should be [move forward] [turn left] [bark].
 You will output the result of each command in a multi-line format, outputting earlier actions first. Each individual action should also include any relevant semantic, location, and situational information.
 For example, if the command is 'hey Pupper, walk forward a little bit, then turn left 90 degrees, then go forward a lot', your output should look like:
-I'll walk forward a little. (newline)
-Then I'll turn left 90 degrees. (newline)
-Then I'll go forward twice as far as I just did. (newline)
+I'll walk forward. (newline)
+Then I'll turn left. (newline)
+Then I'll go forward. (newline)
 DO NOT omit action steps, and DO NOT create action steps when they are not required. SEPARATE EACH LINE WITH A NEWLINE CHARACTER.
 
-UNLESS SPOKEN TO, DO NOT OUTPUT ANY TEXT. YOU ARE SILENT UNTIL ADDRESSED, AND ONLY RESPOND WHEN GIVING SPECIFIC ACTION PLANS IMMEDIATELY AFTER BEING PROMPTED. UNLESS A DIRECT COMMAND IS GIVEN, DO NOT OUTPUT ANY TEXT.
+UNLESS SPOKEN TO, DO NOT OUTPUT ANY TEXT. YOU ARE SILENT UNTIL ADDRESSED, AND ONLY RESPOND WHEN GIVING SPECIFIC ACTION PLANS IMMEDIATELY AFTER BEING PROMPTED. UNLESS A DIRECT COMMAND IS GIVEN, DO NOT OUTPUT ANY TEXT. ALL RESPONSES SHOULD BE UNDER 12 WORDS. 
 
 Your response should be in a .txt file. Keep your response as short as possible without sacrificing any action information.
 
